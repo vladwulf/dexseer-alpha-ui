fb2593e509d87da2f048dce72569354fce76072c
Vladimir Agaev <agaev.vladimir@gmail.com>
2026-02-11 01:57:49 +0100

    chore: prevent alert analysis

--- src/features/alerts-side-panel/AlertsSidePanel.tsx
+++ src/features/alerts-side-panel/AlertsSidePanel.tsx
@@ -9,24 +9,24 @@ import { analyzeAlerts, analyzeAlertsByType } from "./alert-perf";
 export function AlertsSidePanel() {
   const { data: alerts } = useGetAlerts();
 
-  useEffect(() => {
-    if (alerts && alerts?.length > 0) {
-      const longAlerts = alerts.filter((a) => a.type === "VOLUME_SURGE_LONG");
-      const shortAlerts = alerts.filter((a) => a.type === "VOLUME_SURGE_SHORT");
+  // useEffect(() => {
+  //   if (alerts && alerts?.length > 0) {
+  //     const longAlerts = alerts.filter((a) => a.type === "VOLUME_SURGE_LONG");
+  //     const shortAlerts = alerts.filter((a) => a.type === "VOLUME_SURGE_SHORT");
 
-      if (longAlerts.length > 0) {
-        const longResults = analyzeAlerts(longAlerts);
-        console.log("longResults", longResults);
-        console.log(longResults.summary);
-      }
+  //     if (longAlerts.length > 0) {
+  //       const longResults = analyzeAlerts(longAlerts);
+  //       console.log("longResults", longResults);
+  //       console.log(longResults.summary);
+  //     }
 
-      if (shortAlerts.length > 0) {
-        const shortResults = analyzeAlerts(shortAlerts);
-        console.log("shortResults", shortResults);
-        console.log(shortResults.summary);
-      }
-    }
-  }, [alerts]);
+  //     if (shortAlerts.length > 0) {
+  //       const shortResults = analyzeAlerts(shortAlerts);
+  //       console.log("shortResults", shortResults);
+  //       console.log(shortResults.summary);
+  //     }
+  //   }
+  // }, [alerts]);
 
   return (
     <div className="border-border bg-black flex flex-col">
