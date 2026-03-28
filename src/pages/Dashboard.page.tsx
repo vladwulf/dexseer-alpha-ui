c6b6c6ae79d6431a5dcf92522fb49bc11b84c321
Vladimir Agaev <agaev.vladimir@gmail.com>
2026-03-28 18:15:13 +0100

    chore: add market movers

--- src/pages/Dashboard.page.tsx
+++ src/pages/Dashboard.page.tsx
@@ -40,7 +40,9 @@ export function DashboardPage() {
           </div>
 
           <div className="pb-10 pt-14">
-            <div className="my-4 hidden sm:block">{/* <MarketMovers /> */}</div>
+            <div className="my-4 hidden sm:block">
+              <MarketMovers />
+            </div>
 
             {/* <div className="my-10 container mx-auto">
               <ScrollingBanner />
