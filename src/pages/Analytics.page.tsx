9fa78672df2acb70ab3f184dd1492ef6341797ef
Vladimir Agaev <agaev.vladimir@gmail.com>
2026-05-22 01:06:56 +0200

    chore: limit width

--- src/pages/Analytics.page.tsx
+++ src/pages/Analytics.page.tsx
@@ -2,7 +2,7 @@ import { AnalyticsVolume } from "@/features/analytics/AnalyticsVolume";
 
 export function AnalyticsPage() {
   return (
-    <div className="min-h-screen px-4 pb-16 pt-8">
+    <div className="min-h-screen px-4 pb-16 pt-8 max-w-7xl mx-auto">
       <AnalyticsVolume />
     </div>
   );
