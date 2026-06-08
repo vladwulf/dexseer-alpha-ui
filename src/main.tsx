ed5419cd1f50c9855c169bd8b0db469ae1c6b32b
Vladimir Agaev <agaev.vladimir@gmail.com>
2026-06-09 01:05:25 +0200

    chore: add Vercel Web Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
@@ -15,6 +16,7 @@ createRoot(rootElement).render(
   <StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <App />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>,
 );
